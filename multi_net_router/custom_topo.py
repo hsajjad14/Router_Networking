f56357106cf513bbf63deb64d72bc5c0ca37fc87
hsajjad14 <haider.sajjad@mail.utoronto.ca>
2022-03-24 03:16:37 -0400

    start multi-net ospf

---
+++ multi_net_router/custom_topo.py
@@ -0,0 +1,91 @@
+"""
+Inspired by topo-2sw-2host.py from: http://mininet.org/walkthrough/
+Custom mininet topology based on Piazza @249
+
+also: http://csie.nqu.edu.tw/smallko/sdn/mininet_simple_router.html
+
+Simple Router: two switches, each with its own two hosts and one host that is connected to both switches (the "router")
+
+Two simple router's router2 and router1 each conncted to a interface on the monitor node
+
+ host   host  host    host
+   \    /       \   /
+    switch      switch
+   r2-eth0\       /r2-eth1
+           router2
+            |mn-eth1
+    monitor node
+            |mn-eth0
+        router1
+r1-eth0/     \r1-eth1
+   switch    switch
+    / \        / \
+ host host  host host
+
+subnet 10.0.1.1/24, r0-eth0 ip: 10.0.1.1
+subnet 10.0.2.1/24, r0-eth1 ip: 10.0.2.1
+
+
+Adding the 'topos' dict with a key/value pair to generate the newly defined
+topology enables one to pass in '--topo=mytopo' from the command line.
+
+From CLI, run: sudo mn --custom `path to custom_topo.py` --topo mytopo --test pingall
+(the pingall is not necessary; just to test)
+"""
+
+
+from mininet.topo import Topo
+
+class MyTopo (Topo):
+
+        def build(self):
+
+            # router 1
+            r1_eth0 = '10.0.1.1/24'
+            r1_eth1 = '10.0.2.1/24'
+
+            # add hosts and switches
+            left_host_s1 = self.addHost('h1', ip='10.0.1.10/24', defaultRoute='via 10.0.1.1', mac="00:00:00:00:00:01")
+            right_host_s1 = self.addHost('h3', ip='10.0.1.20/24', defaultRoute='via 10.0.1.1', mac="00:00:00:00:00:03")
+            left_host_s2 = self.addHost('h2', ip='10.0.2.10/24', defaultRoute='via 10.0.2.1', mac="00:00:00:00:00:02" )
+            right_host_s2 = self.addHost('h4', ip='10.0.2.20/24', defaultRoute='via 10.0.2.1', mac="00:00:00:00:00:04")
+            r1 = self.addHost('r1')
+            s1 = self.addSwitch('s1')
+            s2 = self.addSwitch('s2')
+
+            # add links
+            self.addLink(left_host_s1, s1)
+            self.addLink(right_host_s1, s1)
+            self.addLink(right_host_s2, s2)
+            self.addLink(left_host_s2, s2)
+            self.addLink(r1, s1)
+            self.addLink(r1, s2)
+
+            # router 2
+            # TODO change all router 2's ip's/subnets/mac addressess
+            # make script for router 2
+            # fix monitor node to connect the two routers
+
+            r2_eth0 = '10.0.1.1/24'
+            r2_eth1 = '10.0.2.1/24'
+
+            # add hosts and switches
+            r2_h1 = self.addHost('h1', ip='10.0.1.10/24', defaultRoute='via 10.0.1.1', mac="00:00:00:00:00:01")
+            r2_h3 = self.addHost('h3', ip='10.0.1.20/24', defaultRoute='via 10.0.1.1', mac="00:00:00:00:00:03")
+            r2_h2 = self.addHost('h2', ip='10.0.2.10/24', defaultRoute='via 10.0.2.1', mac="00:00:00:00:00:02" )
+            r2_h4 = self.addHost('h4', ip='10.0.2.20/24', defaultRoute='via 10.0.2.1', mac="00:00:00:00:00:04")
+            r2 = self.addHost('r1')
+            r2_s1 = self.addSwitch('s1')
+            r2_s2 = self.addSwitch('s2')
+
+            # add links
+            self.addLink(r2_h1, r2_s1)
+            self.addLink(r2_h3, s1)
+            self.addLink(r2_h2, s2)
+            self.addLink(r2_h4, s2)
+            self.addLink(r2, r2_s1)
+            self.addLink(r2, r2_s2)
+        
+
+
+topos = { 'mytopo': ( lambda: MyTopo() ) }
