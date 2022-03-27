8e43ed43cf40d4503f0ceac0b71df5bcd38673c2
hsajjad14 <haider.sajjad@mail.utoronto.ca>
2022-03-27 18:47:07 -0400

    add routing between routers

--- multi_net_router/scripts_for_router2.sh
+++ multi_net_router/scripts_for_router2.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
 echo "Setting up Router"
-
 ifconfig r2-eth0 0
 ifconfig r2-eth2 0
 
 ifconfig r2-eth0 hw ether 00:00:00:00:02:01
 ifconfig r2-eth2 hw ether 00:00:00:00:02:03
-
 ip addr add 10.2.0.1/24 brd + dev r2-eth0
 ip addr add 10.0.6.2/24 brd + dev r2-eth2
 
 echo 1 > /proc/sys/net/ipv4/ip_forward
 
+# adding ip route forwarding
+ip route add 10.0.1.0/24 via 10.0.6.1 dev r2-eth2
+ip route add 10.0.2.0/24 via 10.0.6.1 dev r2-eth2
+ip route add 10.3.0.0/24 via 10.0.4.1 dev r2-eth1
--- multi_net_router/scripts_for_router3.sh
+++ multi_net_router/scripts_for_router3.sh
@@ -1,20 +1,18 @@
 #!/bin/bash
 
 echo "Setting up Router"
-
 ifconfig r3-eth0 0
 ifconfig r3-eth1 0
-ifconfig r3-eth2 0
-ifconfig r3-eth3 0
 
 ifconfig r3-eth0 hw ether 00:00:00:00:03:01
-ifconfig r3-eth1 hw ether 00:00:00:00:03:02
-ifconfig r3-eth2 hw ether 00:00:00:00:03:03
-ifconfig r3-eth2 hw ether 00:00:00:00:03:04
-
+ifconfig r3-eth1 hw ether 00:00:00:00:03:01
 ip addr add 10.3.0.1/24 brd + dev r3-eth0
 ip addr add 10.0.4.2/24 brd + dev r3-eth1
-ip addr add 10.0.3.2/24 brd + dev r3-eth2
-ip addr add 10.0.5.1/24 brd + dev r3-eth3
+
 
 echo 1 > /proc/sys/net/ipv4/ip_forward
+
+# adding routes
+ip route add 10.0.2.0/24 via 10.0.4.2 dev r3-eth1
+ip route add 10.0.1.0/24 via 10.0.4.2 dev r3-eth1
+ip route add 10.2.0.0/24 via 10.0.4.2 dev r3-eth1
