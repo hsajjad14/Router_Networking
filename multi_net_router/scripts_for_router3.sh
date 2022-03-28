6f4d70e6d9a774da0ddb679054c453d9a2d177bb
hsajjad14 <haider.sajjad@mail.utoronto.ca>
2022-03-27 22:43:11 -0400

    fix

--- multi_net_router/scripts_for_router3.sh
+++ multi_net_router/scripts_for_router3.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+
 echo "Setting up Router"
 ifconfig r3-eth0 0
 ifconfig r3-eth1 0
@@ -13,6 +14,8 @@ ip addr add 10.0.4.2/24 brd + dev r3-eth1
 echo 1 > /proc/sys/net/ipv4/ip_forward
 
 # adding routes
-ip route add 10.0.2.0/24 via 10.0.4.2 dev r3-eth1
-ip route add 10.0.1.0/24 via 10.0.4.2 dev r3-eth1
-ip route add 10.2.0.0/24 via 10.0.4.2 dev r3-eth1
+ip route add 10.0.2.0/24 via 10.0.4.1 dev r3-eth1
+ip route add 10.0.1.0/24 via 10.0.4.1 dev r3-eth1
+ip route add 10.2.0.0/24 via 10.0.4.1 dev r3-eth1
+ip route add 10.4.0.0/24 via 10.0.5.2 dev r3-eth3
+
--- multi_net_router/scripts_for_router4.sh
+++ multi_net_router/scripts_for_router4.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+
 echo "Setting up Router"
 
 ifconfig r4-eth0 0
@@ -12,3 +13,9 @@ ip addr add 10.4.0.1/24 brd + dev r4-eth0
 ip addr add 10.0.5.2/24 brd + dev r4-eth1
 
 echo 1 > /proc/sys/net/ipv4/ip_forward
+
+ip route add 10.0.2.0/24 via 10.0.5.1 dev r4-eth1
+ip route add 10.0.1.0/24 via 10.0.5.1 dev r4-eth1
+ip route add 10.2.0.0/24 via 10.0.5.1 dev r4-eth1
+ip route add 10.3.0.0/24 via 10.0.5.1 dev r4-eth1
+
