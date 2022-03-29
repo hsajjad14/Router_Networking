4e5d2a06e8112790c81273f119f142d54e6b45aa
hsajjad14 <haider.sajjad@mail.utoronto.ca>
2022-03-29 18:30:18 -0400

    remove unused class

--- multi_net_router/multi_net_OSPF.py
+++ multi_net_router/multi_net_OSPF.py
@@ -6,16 +6,6 @@ from mininet.log import setLogLevel, info
 from mininet.cli import CLI
 
 
-class LinuxRouter(Node):
-    def config(self, **params):
-        super(LinuxRouter, self).config(**params)
-        self.cmd('sysctl net.ipv4.ip_forward=1')
-
-    def terminate(self):
-        self.cmd('sysctl net.ipv4.ip_forward=0')
-        super(LinuxRouter, self).terminate()
-
-
 class NetworkTopo(Topo):
     def build(self, **_opts):
         # Add 2 routers in two different subnets
